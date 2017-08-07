f9a7fe6c13904e449806a0e3a3481fac7b8022e4
chrisjuin <c.juin.cj@gmail.com>
2017-08-07 14:08:14 +0200

    modif html et js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-function Personne(nom , prenom , adresse , age){
-  
-this.nom = nom ; 
-this.prenom = prenom; 
-this.adresse = adresse; 
-this.age = age;
-  this.affiche = function(){
-  console.log(this.prenom + " " + this.nom + " " +this.adresse + " " +this.age); 
-}
+function Eleve(id , prenom , nom , sexe ,age){
+
+	this.id = id ; 
+	this.prenom = prenom; 
+	this.nom = nom; 
+	this.sexe = sexe;
+	this.age = age
+	this.affiche = function(){
+		console.log(this.id + " " + this.prenom + " " +this.nom + " " +this.sexe + " "); 
+	}
 };
 
-var p1 = new Personne("Juin","Chris" ,"Fanjeaux" , 21);
+var p1 = new Eleve("765","Chris" ,"Juin" , "Masculin" , 21);
 p1.affiche(); 
